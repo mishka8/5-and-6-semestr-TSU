a594c99d87d4d855fdaa63ab324180a9dadc5c41
mishka8 <144250192+mishka8@users.noreply.github.com>
2025-12-20 07:24:14 +0700

    Update tsulab3.c

--- OS/Lab3/tsulab3.c
+++ OS/Lab3/tsulab3.c
@@ -5,6 +5,11 @@
 #include <linux/uaccess.h>
 #include <linux/version.h>
 #include <linux/time.h>
+#include <linux/ktime.h>
+#include <linux/string.h>
+#include <linux/math64.h>
+
+//Расчитайте, когда Солнце покинули лучи света, которыми мы сейчас видим отражёнными от Луны
 
 #define procfs_name "TSULab"
 
@@ -12,9 +17,44 @@ static struct proc_dir_entry *our_proc_file = NULL;
 
 static ssize_t procfile_read(struct file *filePointer, char __user *buffer, size_t buffer_length, loff_t *offset)
 {
-    char s[6] = "Tomsk\n";
-    ssize_t strLen = sizeof(s);
+    struct timespec64 current_time;
+    char s[64];
+    ssize_t strLen;
 
+    ktime_get_real_ts64(&current_time);
+    
+    u32 minutes;
+    u32 seconds;
+
+    u64 earth_moon_distance_m = 384400000ULL;
+    u64 speed_of_light_mps = 299792458ULL;
+    u64 sun_earth_distance_m = 149597870700ULL;
+
+    u64 time_moon_to_earth = div64_u64(earth_moon_distance_m, speed_of_light_mps);
+    u64 time_sun_to_earth = div64_u64(sun_earth_distance_m, speed_of_light_mps);
+    
+    u64 total_travel_time = time_sun_to_earth + time_moon_to_earth; 
+    
+    struct timespec64 sun_departure_time = current_time;
+    
+    if (sun_departure_time.tv_sec >= total_travel_time) 
+    {
+        sun_departure_time.tv_sec -= total_travel_time;
+    } 
+    else 
+    {
+        sun_departure_time.tv_sec = 0;
+    }
+    
+    minutes = total_travel_time / 60;
+    seconds = total_travel_time % 60;
+
+    snprintf(s, sizeof(s), "Total time: %u min %u sec ago\n", minutes, seconds);
+    
+    strLen = strlen(s);
+
+
+    
     if (*offset >= strLen || copy_to_user(buffer, s, strLen)) 
     {
         pr_info("copy_to_user failed\n");
@@ -50,8 +90,9 @@ static int __init procFsInit(void)
         proc_remove(our_proc_file);
         pr_alert("Error: could not initialize /proc/%s\n", procfs_name);
 
-        return -ENOMEM; // не смогли выделить память
+        return -ENOMEM;
     }
+    
     pr_info("Welcome to Tomsk State University\n");
     pr_info("/proc/%s was created\n", procfs_name);
 
@@ -60,7 +101,7 @@ static int __init procFsInit(void)
 
 static void __exit procFsExit(void)
 {
-    proc_remove(our_proc_file); // Удаляем наш файл tsu
+    proc_remove(our_proc_file);
     pr_info("/proc/%s was removed\n", procfs_name);
     pr_info("Tomsk State University forever!\n");
 }
